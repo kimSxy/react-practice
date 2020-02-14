0e169f12221082009f60ac0fe2f63115c951d3de
kimSxy <ashleyx@bu.edu>
2020-02-13 20:51:08 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App {
     let course = [
       new Course('CAS', 'CS', '112', 'B1', 'Sullivan'),
       new Course('CAS', 'EC', '102', 'AA', 'Watson'),
-        new Course('CAS','MA','242','EE','Panth')
+      new Course('CAS','MA','242','EE','Panth')
     ];
 
 
