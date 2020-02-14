e295ef9a140ce8a59211c91a827daf48071802ac
kimSxy <ashleyx@bu.edu>
2020-02-13 21:04:27 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,11 +15,7 @@ class Course{
 class App {
   render() {
 
-    let c = new Course()[
-      new Course('CAS', 'CS', '112', 'B1', 'Sullivan'),
-      new Course('CAS', 'EC', '102', 'AA', 'Watson'),
-      new Course('CAS','MA','242','EE','Panth')
-    ];
+    let c = new Course('CAS', 'CS', '112', 'B1', 'Sullivan');
 
 
     return (
