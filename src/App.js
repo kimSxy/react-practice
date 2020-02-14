fbb943fd4126195819418fe1372d4eda712e1dc1
kimSxy <ashleyx@bu.edu>
2020-02-13 20:46:47 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
+import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Course{
+  constructor(college, department, number, section, professor){
+    this.col = college;
+    this.depart = department;
+    this.num = number;
+    this.sec = section;
+    this.prof = professor;
+  }
+}
+
+class App {
+  render() {
+
+    let course = [
+      new Course('CAS', 'CS', '112', 'B1', 'Sullivan'),
+      new Course('CAS', 'EC', '102', 'AA', 'Watson'),
+        new Course('CAS','MA','242','EE','Panth')
+    ];
+
+
+    return (
+        <ol>
+          {
+            course.map(course => <li>{course.col}{course.depart}{course.num}{course.sec}{course.prof}</li>)
+          }
+        </ol>
+    );
+  }
 }
 
 export default App;
